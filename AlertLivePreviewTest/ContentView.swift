24f196040b02de20ee59889f7d8641c7788edda3
ruslan-qualtie <76435462+ruslan-qualtie@users.noreply.github.com>
2021-06-11 13:17:44 +0300

    Initial commit

---
+++ AlertLivePreviewTest/ContentView.swift
@@ -0,0 +1,26 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Alerts(message: "App Alert")
+    }
+}
+struct Alerts: View {
+    var message: String
+    var body: some View {
+        NavigationView {
+            Text("Where is the Alert?")
+                .alert(isPresented: .constant(true)) {
+                    Alert(title: Text("Hello"), message: Text(message))
+                }
+        }
+    }
+}
+
+#if DEBUG
+struct Alerts_Previews: PreviewProvider {
+    static var previews: some View {
+        Alerts(message: "Preview Alert")
+    }
+}
+#endif
